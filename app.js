3765c180ccc6961715c4a35d8e0c1aa33ad46f61
Ben Pratt <benjaminrpratt@gmail.com>
2020-03-06 11:12:00 -0600

    initial app

---
+++ app.js
@@ -0,0 +1,16 @@
+const { App } = require("@slack/bolt");
+
+const app = new App({
+    token: process.env.SLACK_BOT_TOKEN,
+    signingSecret: process.env.SLACK_SIGNING_SECRET
+});
+
+app.message('hi', ({ message, say }) => {
+    say(`Hey there <@${message.user}>!`);
+});
+
+(async () => {
+    await app.start(process.env.PORT || 3000);
+
+    console.log('Slack moderation app is running!');
+})();
